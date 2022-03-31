dfe2d96209b6f8cbd9fa407bba8d16840c8305ac
Marcos <m_vfonseca@hotmail.com>
2022-03-31 20:43:00 -0300

    first commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,8 +6,8 @@ import br.com.dio.model.Gato;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
-        System.out.println(gato);
 
+        System.out.println(gato);
 
 /*        int a = 5;
         int b = 3;
